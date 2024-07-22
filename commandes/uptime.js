60abf840c984c8a7f6ac861b390fa8ca49342da7
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-22 21:13:26 +0300

    Update uptime.js

--- commandes/uptime.js
+++ commandes/uptime.js
@@ -28,7 +28,7 @@ zokou({ nomCom: 'uptime',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
 
-                 await repondre(`*_uptime of joel md bot is: ${runtime(process.uptime())}_*`) 
+                 await repondre(`*_uptime of  Bugatti is: ${runtime(process.uptime())}_*`) 
 
    
 
@@ -57,7 +57,7 @@ let linkkk = `https://api.screenshotmachine.com/?key=c04d3a&url=${encodeURICompo
 
 let res = await getBuffer(linkkk);
 
-await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 * JOEL BOT*'}, { quoted: ms });
+await zk.sendMessage(dest, { image: res, caption: '𝑾𝒆𝒃 𝑺𝒄𝒓𝒆𝒆𝒏𝒔𝒉𝒐𝒕 𝒃𝒚 *Bugatti*'}, { quoted: ms });
 
 
 }
