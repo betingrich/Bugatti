e303503e915051f196285e834785ce850e4994e7
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-25 08:49:36 +0300

    set.js

--- set.js
+++ set.js
@@ -10,7 +10,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || 'zokk',
     PREFIXE: process.env.PREFIX || ".",
     OWNER_NAME: process.env.OWNER_NAME || "King Marisel",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || "25440007567",              
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "254740007567",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "non",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'non',
     BOT : process.env.BOT_NAME || '𝑩𝑼𝑮𝑨𝑻𝑻𝑰',
