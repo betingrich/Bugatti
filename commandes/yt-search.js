1814722dc698f08987ace89b447017a5ecdf6a8e
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-27 00:47:10 +0300

    Update yt-search.js

--- commandes/yt-search.js
+++ commandes/yt-search.js
@@ -19,9 +19,9 @@ zokou({ nomCom: "yts", categorie: "Search", reaction: "🐅" }, async (dest, zk,
 
     let captions = "";
 for (let i = 0; i < 15; i++) {
-  captions += `ᴊᴏᴇʟ ᴍᴅ\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
+  captions += `𝑩𝒖𝒈𝒂𝒕𝒕𝒊\n${i + 1}. Title: ${resultat[i].title}\nTime : ${resultat[i].timestamp}\nUrl: ${resultat[i].url}\n`;
 }
-    captions += "\n======\n*powered by joel kang'oma*";
+    captions += "\n======\n*𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝑩𝒚 𝑩𝒖𝒈𝒂𝒕𝒕𝒊*";
 
     // repondre(captions)
     zk.sendMessage(dest, { image: { url: resultat[0].thumbnail }, caption: captions }, { quoted: ms });
