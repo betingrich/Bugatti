9ae70e91e6e93a521743497c33c7b1ba90162970
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-26 23:57:56 +0300

    ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -7,7 +7,7 @@ const { default: axios } = require('axios');
 zokou({ nomCom: 'ping',
     desc: 'To check ping',
     Categorie: 'General',
-    reaction: '🍑', 
+    reaction: '🏎️', 
     fromMe: 'true', 
 
        
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*Bugatti Response Speed*\n ```' + 9999 + '``` *m/s*') 
+    return repondre('*𝑩𝒖𝒈𝒂𝒕𝒕𝒊 𝒓𝒆𝒔𝒑𝒐𝒏𝒔𝒆 𝑺𝒑𝒆𝒆𝒅*\n ```' + ▱▱▱▰▰▰▰ :0.0013 MS* + '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
