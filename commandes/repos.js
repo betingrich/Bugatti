b3592adef44f2b4fa47f24942a2b44c1157faff7
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-26 23:23:28 +0300

    repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -21,15 +21,14 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __f
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `*hellow whatsaap user
-this is* *𝑩𝑼𝑮𝑨𝑻𝑻𝑰.*\n get session id *by*, *pairing code*  https://joelsession1-4a8c04ad2935.herokuapp.com/pair/
+      const gitdata = `*𝑯𝒆𝒍𝒍𝒐 𝑴𝒚 𝒖𝒔𝒆𝒓 𝑻𝒉𝒊𝒔 𝒊𝒔* *𝑩𝑼𝑮𝑨𝑻𝑻𝑰.*\n 𝒈𝒆𝒕 𝒔𝒆𝒔𝒔𝒊𝒐𝒏 𝒊𝒅 *𝒐𝒓*, *𝒑𝒂𝒓𝒊𝒏𝒈 𝑪𝒐𝒅𝒆*  https://joelsession1-4a8c04ad2935.herokuapp.com/pair/
 
-🕷️ *REPOSITORY:* ${data.html_url}
-🕷️ *STARS:* ${repoInfo.stars}
-🕷️ *FORKS:* ${repoInfo.forks}
-🕷️ *RELEASE DATE:* ${releaseDate}
-🕷️ *UPDATE ON:* ${repoInfo.lastUpdate}
-🕷️ *OWNER:* *𝑲𝑰𝑵𝑮 𝑴𝑨𝑹𝑰𝑺𝑬𝑳*
+🕷️ *𝑹𝒆𝒑𝒐𝒔𝒊𝒕𝒐𝒓𝒚:* ${data.html_url}
+🕷️ *𝑺𝒕𝒂𝒓𝒔:* ${repoInfo.stars}
+🕷️ *𝑭𝒐𝒓𝒌𝒔:* ${repoInfo.forks}
+🕷️ *𝑹𝒆𝒍𝒆𝒂𝒔𝒆 𝑫𝒂𝒕𝒆:* ${releaseDate}
+🕷️ *𝑼𝒑𝒅𝒂𝒕𝒆 𝑶𝒏:* ${repoInfo.lastUpdate}
+🕷️ *𝑶𝒘𝒏𝒆𝒓:* *𝑲𝑰𝑵𝑮 𝑴𝑨𝑹𝑰𝑺𝑬𝑳*
 __________________________________
             *𝑩𝑼𝑮𝑨𝑻𝑻𝑰 𝑪𝑹𝑬𝑨𝑻𝑰𝑶𝑵*`;
 
