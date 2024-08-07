e6182f4a25bcc9d62d25d29db7493d9fec13bbd6
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-07 20:45:06 +0300

    Marisel

--- commandes/menu.js
+++ commandes/menu.js
@@ -67,21 +67,21 @@ const date = moment().format('DD/MM/YYYY');
   let infoMsg =  `
 
 ┏━━━◈𝑩𝑼𝑮𝑨𝑻𝑻𝑰◈━━
-┃ ☄Mode: ${mode}
-┃ ☄User : ${s.OWNER_NAME}
-┃ ☄Library : Baileys
-️┃ ☄Prefix : ${s.PREFIXE}
-️┃ ☄Date : ${date}
-┃ ☄Time : ${temps}
-┃ ☄Tools : ${cm.length}
-┃ ☄Ram : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-┃ ☄Host : ${os.platform()}
-┃ ☄ Mode: ${mode}
+┃ ⿻Mode: ${mode}
+┃ ⿻User : ${s.OWNER_NAME}
+┃ ⿻Library : Baileys
+️┃ ⿻Prefix : ${s.PREFIXE}
+️┃ ⿻Date : ${date}
+┃ ⿻Time : ${temps}
+┃ ⿻Tools : ${cm.length}
+┃ ⿻Ram : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
+┃ ⿻Host : ${os.platform()}
+┃ ⿻Mode: ${mode}
 ┗━━━◈𝑩𝑼𝑮𝑨𝑻𝑻𝑰◈━━ \n\n`;
 
 
 let menuMsg = `
-  ☄𝑩𝑼𝑮𝑨𝑻𝑻𝑰☄
+  𝑩𝑼𝑮𝑨𝑻𝑻𝑰
 `;
 
 
@@ -104,8 +104,8 @@ let menuMsg = `
 
     menuMsg += `
 ︎
-️    ☄𝑩𝑼𝑮𝑨𝑻𝑻𝑰☄
-  ☄𝑺𝒖𝒑𝒆𝒓 𝑭𝒂𝒔𝒕 𝑩𝒐𝒕☄
+️    𝑩𝑼𝑮𝑨𝑻𝑻𝑰
+  𝑺𝒖𝒑𝒆𝒓 𝑭𝒂𝒔𝒕 𝑩𝒐𝒕
 
 ┏━━━━━━━━━━━━━━┓
 ┃𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝒃𝒚 𝑩𝒖𝒈𝒂𝒕𝒕𝒊.
