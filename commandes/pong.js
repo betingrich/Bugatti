261fcd74445bd5bf015d5c9dc60b56364b20743f
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-22 02:01:56 +0300

    Marisel

--- commandes/pong.js
+++ commandes/pong.js
@@ -33,7 +33,7 @@ zokou(
     const end = new Date().getTime();
     const ping = end - start;
     await zk.sendMessage(dest, {
-      text: `Pong! ${ping}ms 🫥`, edit: {
+      text: ` *Pong! ${ping}ms* 🫥`, edit: {
         id: msg.key.id, remoteJid: dest
       }});
     await zk.sendMessage(dest, {
