bc2ec57a90b3407b94cd19ea2da3dff91ceaf210
marisel <140407389+betingrich@users.noreply.github.com>
2024-08-28 19:03:01 +0300

    Marisel

--- commandes/repos.js
+++ commandes/repos.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
-zokou({ nomCom: "repo", catégorie:"Général", reaction: "📑", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "repo", catégorie:"Général", reaction: "🔎", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
   const githubRepo = 'https://api.github.com/repos/betingrich/bugatti';
   const img = 'https://telegra.ph/file/46131539229f7e5614ea7.jpg';
 
