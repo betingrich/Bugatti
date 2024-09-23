d519e40e0467dd26499a63cdad24326cbf598062
𝚓𝚘𝚎𝚕 𝚓𝚊𝚖𝚎𝚜 <joeljamestech3@gmail.com>
2024-09-23 21:35:35 +0300

    𝙻𝚘𝚛𝚍 𝚓𝚘𝚎𝚕

--- framework/mesfonctions.js
+++ framework/mesfonctions.js
@@ -9,7 +9,7 @@ const baileys_1 = require("@whiskeysockets/baileys");
 const fs = require('fs-extra');
 const util = require('util');
 let { listall } = require('./stylish-font');
-/*_________by Marisel 
+/*_________by Boniphace
 
 fonction zJson:
 récupère un objet json
@@ -32,7 +32,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'Bugatti',
+        pack: 'BONIPHACE-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
