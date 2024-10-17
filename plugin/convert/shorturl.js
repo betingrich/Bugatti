acec27b7527f718a1fd927f50079786f1745b786
marisel <140407389+betingrich@users.noreply.github.com>
2024-10-17 16:53:28 +0300

    Bugatti

---
+++ plugin/convert/shorturl.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+require("../../config");
+
+module.exports = {
+    type: 'convert',
+    command: ['shorten', 'shortlink', 'shorturl', 'shortenlink'],
+    operate: async (context) => {
+        const { sam, m, q, prefix, command, reaction, reply } = context;
+
+        if (!q) {
+            await reply(`Add input, Example: *${prefix + command} https://example.com*`);
+            await reaction(m.chat, "❗");
+            return;
+        }
+
+        try {
+            await reaction(m.chat, "🪄");
+            const { data: tinyUrlShortUrl } = await axios.get(`https://tinyurl.com/api-create.php?url=${q}`);
+            await reply(`Shortened Succes\n\n*TinyURL: ${tinyUrlShortUrl}*`);
+            await reaction(m.chat, "🚗");
+        } catch (error) {
+            console.error('Error shortening URL:', error);
+            await xreply('Failed to shorten URL. Please try again later.');
+            await reaction(m.chat, "❌");
+        }
+    }
+};
