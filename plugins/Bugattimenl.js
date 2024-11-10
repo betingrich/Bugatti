dc4800bfa8e57452585b33af55870e6a05191cbc
marisel <140407389+betingrich@users.noreply.github.com>
2024-11-10 20:27:06 +0300

    Bugatti

---
+++ plugins/Bugattimenl.js
@@ -0,0 +1,352 @@
+let handler = async (m, { conn, usedPrefix, command }) => {
+  let who = m.quoted
+    ? m.quoted.sender
+    : m.mentionedJid && m.mentionedJid[0]
+      ? m.mentionedJid[0]
+      : m.fromMe
+        ? conn.user.jid
+        : m.sender
+  if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
+
+  let pp = './Assets/Bugatti.jpg'
+  let more = String.fromCharCode(8206)
+  let readMore = more.repeat(850)
+
+  let lkr
+  switch (command) {
+    case 'list':
+      lkr =
+        '*Get ready for the ride, here are your ticket options:*\n\n' +
+        '🤖 *' +
+        usedPrefix +
+        "Botmenu* - Get full Bot Comands here.\n\n" +
+        '👑 *' +
+        usedPrefix +
+        "Ownermenu* - This is personal for Marisel\n\n" +
+        '🧑‍🤝‍🧑 *' +
+        usedPrefix +
+        'Groupmenu* - Use these commands in Groups only!\n\n' +
+        '📥 *' +
+        usedPrefix +
+        "Dlmenu* - 'Download anything on this section!\n\n" +
+        '🎉 *' +
+        usedPrefix +
+        "Funmenu* - Ride along play with frienda and challenge yourself with these games!\n\n" +
+        '💰 *' +
+        usedPrefix +
+        'Economymenu* - Collect coins daily and you can purchase with !\n\n' +
+        '🎮 *' +
+        usedPrefix +
+        'glGamemenu* - Enter the gaming arena. May the odds be ever in your favor!\n\n' +
+        '🎨 *' +
+        usedPrefix +
+        'Stickermenu* - A rainbow of stickers for your inner artist. Make your chats pop!\n\n' +
+        '🧰 *' +
+        usedPrefix +
+        "Toolmenu* - Your handy-dandy toolkit. What's your pick, genius?\n\n" +
+        '🎩 *' +
+        usedPrefix +
+        'Logomenu* - Create a logo that screams YOU. Or whispers. You choose the volume.\n\n' +
+        '🌙 *' +
+        usedPrefix +
+        'Nsfwmenu* - The After Dark menu. But remember, sharing adult secrets must be consent-based.'
+      break
+
+    case 'bugattimenu':
+      lkr = `° ┄──•••───╮
+          *Bugatti Menu
+   ╰───•••──┄ °     
+   ┏━━━ʕ•㉨•ʔ━━━┓
+   ⎪┃  _${usedPrefix}gita_
+   ⎪┃  _${usedPrefix}ping_
+   ⎪┃  _${usedPrefix}uptime_
+   ⎪┃  _${usedPrefix}bot_
+   ⎪┃  _${usedPrefix}owner_
+   ⎪┃  _${usedPrefix}script_
+   ⎪┃  _${usedPrefix}runtime_
+   ⎪┃  _${usedPrefix}infobot_
+   ⎪┃  _${usedPrefix}donate_
+   ⎪┃  _${usedPrefix}groups_
+   ⎪┃  _${usedPrefix}blocklist_
+   ⎪┃  _${usedPrefix}listprem_  
+   ┗━━━ʕ•㉨•ʔ━━━┛` // Your bot menu message here
+      break
+    case 'mariselmenu':
+      lkr = `° ┄──•••───╮
+          *Marisel Menu* 
+     ╰───•••──┄ °     
+     ┏━━━ʕ•㉨•ʔ━━━┓
+     ⎪┃  _${usedPrefix}banchat_
+     ⎪┃  _${usedPrefix}unbanchat_
+     ⎪┃  _${usedPrefix}banuser_
+     ⎪┃  _${usedPrefix}unbanuser_
+     ⎪┃  _${usedPrefix}Broadcast_
+     ⎪┃  _${usedPrefix}Broadcastgc_
+     ⎪┃  _${usedPrefix}join_
+     ⎪┃  _${usedPrefix}setppbot_
+     ⎪┃  _${usedPrefix}setprefix_
+     ⎪┃  _${usedPrefix}resetprefix_
+     ⎪┃  _${usedPrefix}getfile_
+     ⎪┃  _${usedPrefix}getplugin_
+     ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'groupmenu':
+      lkr = `° ┄──•••───╮
+          *Group Menu*
+    ╰───•••──┄ °     
+    ┏━━━ʕ•㉨•ʔ━━━┓
+    ⎪┃ _${usedPrefix}kick *<@tag>*_
+    ⎪┃ _${usedPrefix}promote *<@tag>*_
+    ⎪┃  _${usedPrefix}demote *<@tag>*_
+    ⎪┃  _${usedPrefix}infogroup_
+    ⎪┃  _${usedPrefix}resetlink_
+    ⎪┃  _${usedPrefix}link_
+    ⎪┃  _${usedPrefix}setpp *<image>*_
+    ⎪┃  _${usedPrefix}setname *<text>*_
+    ⎪┃  _${usedPrefix}setdesc *<text>*_
+    ⎪┃  _${usedPrefix}setwelcome *<text>*_
+    ⎪┃  _${usedPrefix}setbye *<text>*_
+    ⎪┃  _${usedPrefix}hidetag *<text/image/audio/vid>*_
+    ⎪┃  _${usedPrefix}warn *<@tag>*_
+    ⎪┃  _${usedPrefix}unwarn *<@tag>*_
+    ⎪┃  _${usedPrefix}group *<open/close>*_
+    ⎪┃  _${usedPrefix}enable
+    ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'downloadermenu':
+    case 'dlmenu':
+      lkr = `° ┄──•••───╮
+          *Download Menu*
+   ╰───•••──┄ °     
+   ┏━━━ʕ•㉨•ʔ━━━┓
+   ⎪┃ _${usedPrefix}play_
+   ⎪┃ _${usedPrefix}song_
+   ⎪┃  _${usedPrefix}yta <link>_
+   ⎪┃  _${usedPrefix}ytv <link>_
+   ⎪┃  _${usedPrefix}ytmp3 <link>_
+   ⎪┃ _${usedPrefix}ytmp4 <link>_
+   ⎪┃  _${usedPrefix}gimage_
+   ⎪┃  _${usedPrefix}pinterest_
+   ⎪┃  _${usedPrefix}mediafire <link>_
+   ⎪┃  _${usedPrefix}gdrive <link>_
+   ⎪┃  _${usedPrefix}gitclone <link>_
+   ⎪┃  _${usedPrefix}twitter <link>_
+   ⎪┃  _${usedPrefix}tiktok <link>_
+   ⎪┃  _${usedPrefix}tiktokstalk_
+   ⎪┃  _${usedPrefix}instagram <link>_
+   ⎪┃  _${usedPrefix}spotify_
+   ⎪┃  _${usedPrefix}facebook <link>_
+   ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'economymenu':
+      lkr = `° ┄──•••───╮
+          *Economy Menu*
+   ╰───•••──┄ °     
+   ┏━━━ʕ•㉨•ʔ━━━┓
+   ⎪┃  _${usedPrefix}claim/daily_
+   ⎪┃  _${usedPrefix}weekly_
+   ⎪┃  _${usedPrefix}monthly_
+   ⎪┃  _${usedPrefix}leaderboard_
+   ⎪┃  _${usedPrefix}bet_
+   ⎪┃  _${usedPrefix}heal_
+   ⎪┃  _${usedPrefix}craft_
+   ⎪┃  _${usedPrefix}balance_
+   ⎪┃  _${usedPrefix}shop_
+   ⎪┃  _${usedPrefix}sell_
+   ⎪┃  _${usedPrefix}adventure_
+   ⎪┃  _${usedPrefix}opencrate_
+   ⎪┃  _${usedPrefix}mine_
+   ⎪┃  _${usedPrefix}work_
+   ⎪┃  _${usedPrefix}transfer_
+   ⎪┃  _${usedPrefix}todiamond_
+   ⎪┃  _${usedPrefix}tomoney_
+   ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'funmenu':
+      lkr = `° ┄──•••───╮
+          *Fun Menu*
+    ╰───•••──┄ °     
+    ┏━━━ʕ•㉨•ʔ━━━┓
+    ⎪┃  _${usedPrefix}character_
+    ⎪┃  _${usedPrefix}truth_
+    ⎪┃  _${usedPrefix}dare_
+    ⎪┃  _${usedPrefix}flirt_
+    ⎪┃  _${usedPrefix}gay_
+    ⎪┃  _${usedPrefix}shayeri_
+    ⎪┃  _${usedPrefix}ship_
+    ⎪┃  _${usedPrefix}waste_
+    ⎪┃  _${usedPrefix}simpcard_
+    ⎪┃  _${usedPrefix}hornycard_
+    ⎪┃  _${usedPrefix}ytcomment_
+    ⎪┃  _${usedPrefix}stupid_
+    ⎪┃  _${usedPrefix}lolicon_
+    ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'animemenu':
+      lkr = `° ┄──•••───╮
+          *Anime Menu*
+    ╰───•••──┄ °     
+    ┏━━━ʕ•㉨•ʔ━━━┓
+    ⎪┃  _${usedPrefix}waifu_
+    ⎪┃  _${usedPrefix}neko_
+    ⎪┃  _${usedPrefix}loli_
+    ⎪┃  _${usedPrefix}couplepp_
+    ⎪┃  _${usedPrefix}toanime_
+    ⎪┃  _${usedPrefix}naruto_
+    ⎪┃  _${usedPrefix}itachi_
+    ⎪┃  _${usedPrefix}akira_
+    ⎪┃  _${usedPrefix}asuna_
+    ⎪┃  _${usedPrefix}akiyama_
+    ⎪┃  _${usedPrefix}boruto_
+    ⎪┃  _${usedPrefix}hornycard_
+    ⎪┃  _${usedPrefix}ayuzawa_
+    ⎪┃  _${usedPrefix}anna_
+    ⎪┃  _${usedPrefix}chiho_
+    ⎪┃  _${usedPrefix}chitoge_
+    ⎪┃  _${usedPrefix}deidara_
+    ⎪┃  _${usedPrefix}erza_
+    ⎪┃  _${usedPrefix}elaina_
+    ⎪┃  _${usedPrefix}emilia_
+    ⎪┃  _${usedPrefix}hestia_
+    ⎪┃  _${usedPrefix}hinata_
+    ⎪┃  _${usedPrefix}inori_
+    ⎪┃  _${usedPrefix}isuzu_
+    ⎪┃  _${usedPrefix}kagura_
+    ⎪┃  _${usedPrefix}kaori_
+    ⎪┃  _${usedPrefix}keneki_
+    ⎪┃  _${usedPrefix}kurumi_
+    ⎪┃  _${usedPrefix}madara_
+    ⎪┃  _${usedPrefix}mikasa_
+    ⎪┃  _${usedPrefix}miku_
+    ⎪┃  _${usedPrefix}minato_
+    ⎪┃  _${usedPrefix}nezuko_
+    ⎪┃  _${usedPrefix}sagiri_
+    ⎪┃  _${usedPrefix}sasuke_
+    ⎪┃  _${usedPrefix}sakura_
+    ⎪┃  _${usedPrefix}kotori_
+    ┗━━━ʕ•㉨•ʔ━━━┛
+    `
+      break
+    case 'gamemenu':
+      lkr = `° ┄──•••───╮
+          *Game Menu*
+    ╰───•••──┄ °     
+    ┏━━━ʕ•㉨•ʔ━━━┓
+    ⎪┃  _${usedPrefix}tictactoe_
+    ⎪┃  _${usedPrefix}delttt_
+    ⎪┃  _${usedPrefix}math_
+    ⎪┃  _${usedPrefix}math answer_
+    ⎪┃  _${usedPrefix}ppt_
+    ⎪┃  _${usedPrefix}slot_
+    ⎪┃  _${usedPrefix}casino_
+    ⎪┃  _${usedPrefix}yourmom_
+    ⎪┃  _${usedPrefix}teri mummy_
+    ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'stickermenu':
+      lkr = `° ┄──•••───╮
+          *Sticker Menu*
+     ╰───•••──┄ °     
+     ┏━━━ʕ•㉨•ʔ━━━┓
+     ⎪┃  _${usedPrefix}sticker_
+     ⎪┃  _${usedPrefix}take_
+     ⎪┃  _${usedPrefix}scircle_
+     ⎪┃  _${usedPrefix}smaker_
+     ⎪┃  _${usedPrefix}sremovebg_
+     ⎪┃  _${usedPrefix}getsticker_
+     ⎪┃  _${usedPrefix}emojimix_
+     ⎪┃  _${usedPrefix}toimg_
+     ⎪┃  _${usedPrefix}tovid_
+     ⎪┃  _${usedPrefix}ttp_
+     ⎪┃  _${usedPrefix}telesticker_
+     ⎪┃  _${usedPrefix}attp_
+     ⎪┃  _${usedPrefix}attp2_
+     ⎪┃  _${usedPrefix}attp3_
+     ┗━━━ʕ•㉨•ʔ━━━┛`
+      break
+    case 'toolmenu':
+      lkr = `° ┄──•••───╮
+          *Tools Menu*
+     ╰───•••──┄ °     
+     ┏━━━ʕ•㉨•ʔ━━━┓
+     ⎪┃  _${usedPrefix}autosticker_
+     ⎪┃  _${usedPrefix}pdf_
+     ⎪┃  _${usedPrefix}whatmusic_
+     ⎪┃  _${usedPrefix}calc_
+     ⎪┃  _${usedPrefix}google_
+     ⎪┃  _${usedPrefix}lyrics_
+     ⎪┃  _${usedPrefix}readmore_
+     ⎪┃  _${usedPrefix}ssweb_
+     ⎪┃  _${usedPrefix}tts_
+     ⎪┃  _${usedPrefix}translate_
+     ⎪┃  _${usedPrefix}tourl_
+     ⎪┃  _${usedPrefix}wikipedia_
+     ⎪┃  _${usedPrefix}nowa_
+     ⎪┃  _${usedPrefix}qrmaker_
+     ⎪┃  _${usedPrefix}readqr_
+     ⎪┃  _${usedPrefix}fancy_
+     ⎪┃  _${usedPrefix}weather_
+     ⎪┃  _${usedPrefix}siri_
+     ⎪┃  _${usedPrefix}alexa_
+     ⎪┃  _${usedPrefix}dalle_
+     ⎪┃  _${usedPrefix}tocartoon_
+     ⎪┃  _${usedPrefix}quote_
+     ⎪┃  _${usedPrefix}technews_
+     ⎪┃  _${usedPrefix}define_
+     ⎪┃  _${usedPrefix}pokedex_
+     ⎪┃  _${usedPrefix}removebg_
+     ⎪┃  _${usedPrefix}apk_
+     ⎪┃  _${usedPrefix}tinyurl/shorturl_
+     ⎪┃  _${usedPrefix}readvo_
+     ⎪┃  _${usedPrefix}true_
+     ┗━━━ʕ•㉨•ʔ━━━┛` //
+      break
+    case 'nsfwmenu':
+      lkr = `use command ${usedPrefix}nsfw` //
+      break
+    case 'logomenu':
+      lkr = `use ${usedPrefix}logo to see all options \ngfx cmd upto 12` //
+      break
+    default:
+      lkr = `Invalid command. Type ${usedPrefix}list to see available options.`
+  }
+
+  conn.sendFile(m.chat, pp, 'perfil.jpg', lkr, m, false, { mentions: [who] })
+
+  let done = '🐥'
+  m.react(done)
+}
+
+handler.help = [
+  'list',
+  'botmenu',
+  'ownermenu',
+  'groupmenu',
+  'dlmenu',
+  'downloadermenu',
+  'economymenu',
+  'funmenu',
+  'gamemenu',
+  'stickermenu',
+  'nsfwmenu',
+  'logomenu',
+  'toolmenu',
+]
+handler.tags = ['main']
+handler.command = [
+  'list',
+  'botmenu',
+  'ownermenu',
+  'groupmenu',
+  'dlmenu',
+  'downloadermenu',
+  'economymenu',
+  'funmenu',
+  'gamemenu',
+  'stickermenu',
+  'nsfwmenu',
+  'logomenu',
+  'toolmenu',
+]
+
+export default handler
